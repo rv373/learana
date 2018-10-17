3d728da3349ce4e8c98a4e85ff4f4aac16b425ed
rv373 <rv373@njit.edu>
2018-10-16 21:50:22 -0400

    Laravel setup

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+<h1 align="center">Hello world Welcome to my About Page</h1>
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+<h1 align="center">Hello world Welcome to my Homepage</h1>
+
+@stop
